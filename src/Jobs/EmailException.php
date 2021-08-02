af404f7f16b23f6cd5644d27a3284568d385dc9c
bastinald <bastinald@icloud.com>
2021-08-02 02:39:33 -0400

    no loop

--- src/Jobs/EmailException.php
+++ src/Jobs/EmailException.php
@@ -7,7 +7,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Mail\Message;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Mail;
 
 class EmailException implements ShouldQueue
@@ -24,17 +23,14 @@ class EmailException implements ShouldQueue
 
     public function handle()
     {
-        $emails = Arr::wrap(config('laravel-exception-emailer.emails'));
-
-        if (!$emails || !app()->environment(config('laravel-exception-emailer.environments'))) {
+        if (!config('laravel-exception-emailer.emails') ||
+            !app()->environment(config('laravel-exception-emailer.environments'))) {
             return;
         }
 
-        foreach ($emails as $email) {
-            Mail::html($this->html, function (Message $message) use ($email) {
-                $message->subject($this->subject);
-                $message->to($email);
-            });
-        }
+        Mail::html($this->html, function (Message $message) {
+            $message->subject($this->subject);
+            $message->to(config('laravel-exception-emailer.emails'));
+        });
     }
 }
